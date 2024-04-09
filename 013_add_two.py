d6c458b06a8011af963f34ec46443e32b2537204
Ehtesham Ali Khan <ehtesham5144@gmail.com>
2024-04-10 00:58:50 +0100

    Update 013_add_two.py

--- 013_add_two.py
+++ 013_add_two.py
@@ -12,7 +12,10 @@ from lib.helpers import check_that_these_are_equal
 
 # YOUR FUNCTION GOES BELOW THIS LINE
 
+def two(n):
+  return n + 1
 
+print(two(3))
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
 
